127be1f6bec60512b957e35be695ee24b438f215
Lorena <lorenacapraru@gmail.com>
2023-03-19 12:05:27 +0000

    Style Level 1

--- job-listings-react/src/Cards.jsx
+++ job-listings-react/src/Cards.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 export default function Cards(props) {
   function createCard(card) {
-
     return (
       <div className="card">
         <div className="logo">
@@ -12,16 +11,24 @@ export default function Cards(props) {
           </div>
           <div className="details">
             <div className="DetailsRow1">
-              {card.company} {card.new} {card.featured}
+              <span className="company">{card.company}</span>{" "}
+              {card.new ? <span className="new">NEW!</span> : null}
+              {card.featured ? (
+                <span className="featured">FEATURED</span>
+              ) : null}
             </div>
             <div className="DetailsRow2">{card.position}</div>
             <div className="DetailsRow3">
-              {card.postedAt} {card.contract} {card.location}
+              <ul>
+                <li className="firstLi">{card.postedAt}</li>{" "}
+                <li>{card.contract}</li> <li> {card.location}</li>
+              </ul>
             </div>
           </div>
         </div>
         <div className="languages">
-          {card.role} {card.level} {card.languages}{" "}
+          <span>{card.role}</span> <span>{card.level}</span>{" "}
+          <span>{card.languages}</span>
         </div>
       </div>
     );
